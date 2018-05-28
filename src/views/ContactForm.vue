d7da4bcf0caafcea0df237d40a5d6ca0ee94268f
jordanhudgens <jordan@devcamp.com>
2018-05-28 16:18:02 -0500

    Added model binding to the contact form

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -3,9 +3,9 @@
     <h2>Contact Us</h2>
     <form @submit.prevent="submitContactForm">
 
-      <input type="text" placeholder="Full Name">
-      <input type="text" placeholder="Email">
-      <textarea name="contactMessage" cols="30" rows="10" placeholder="Message"></textarea>
+      <input type="text" v-model="fullName" placeholder="Full Name">
+      <input type="text" v-model="email" placeholder="Email">
+      <textarea name="contactMessage" v-model="contactMessage" cols="30" rows="10" placeholder="Message"></textarea>
 
       <button type="submit">Send Message</button>
     </form>
@@ -13,11 +13,22 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'ContactForm',
+  data() {
+    return {
+      fullName: '',
+      email: '',
+      contactMessage: ''
+    }
+  },
   methods: {
     submitContactForm() {
-      console.log('Form Submittedd...');
+      console.log(this.fullName);
+      console.log(this.email);
+      console.log(this.contactMessage);
     }
   }
 }
